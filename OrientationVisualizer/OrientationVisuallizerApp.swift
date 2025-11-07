f25018991a45510076cdc3b1bf9bfee22c3519c0
Tom Chau <tung.t.t.chau@gmail.com>
2025-11-06 17:17:55 -0800

    Initial Commit

---
+++ OrientationVisualizer/OrientationVisuallizerApp.swift
@@ -0,0 +1,12 @@
+//
+//  OrientationVisuallizerApp.swift
+//
+import SwiftUI
+
+struct OrientationVisualizer: View {
+    var body: some Scene {
+        WindowGroup {
+            OrientationRootView()
+        }
+        
+    }
